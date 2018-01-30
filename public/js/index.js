157e6dfb0ac92331223ccebd6b15e4575f4574ec
Irene Nicolini <inicolini@iks.local>
2018-01-30 16:55:12 +0100

    form per inviare messaggi e lista messaggi ricevuti

--- public/js/index.js
+++ public/js/index.js
@@ -12,9 +12,22 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (msg) {
     console.log(`New message from ${msg.from} at ${msg.createdAt}: ${msg.text}`);
-    
+    var li = jQuery('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+    jQuery('#messages').append(li);
 })
 
-socket.emit('createMessage', {from: 'Frank', text:'hello there!'}, function(data) {
-    console.log('got it!', data);
-});
+// socket.emit('createMessage', {from: 'Frank', text:'hello there!'}, function(data) {
+//     console.log('got it!', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+})
