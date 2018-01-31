ebb1cbea757e976c3d821b27625833058331cf08
Irene Nicolini <inicolini@iks.local>
2018-01-31 09:58:01 +0100

    styling

--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,19 @@ socket.on('newLocationMessage', function (message) {
 });
 
 jQuery('#message-form').on('submit', function (e) {
+
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        // clear value after sending message
+        messageTextbox.val('');
     });
+
 })
 
 // gestione bottone location
@@ -43,15 +48,20 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocalizzazione non supportata dal browser');
     }
-
+    //dopo averlo cliccato viene disabilitato
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+    
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position);
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        //abilito di nuovo il bottone
+        locationButton.removeAttr('disabled').text('Send Location');
     }, function (err) {
         alert('Unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send Location');
     })
 
 });
